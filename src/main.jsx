bfc11d54d5558d98812703d4ac9d2f586521c996
sanlk21 <sandunlakmal2001@gmail.com>
2025-01-13 20:12:24 +0530

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Import global styles
 import './assets/styles/global.css';
+import './assets/styles/index.css';
 import './assets/styles/variables.css';
 
 // Optional: Import and configure i18n for internationalization
